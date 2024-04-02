b2e6ec590d9f38fd41fce2da0a946cdecf2cd268
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-01 22:22:35 -0500

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    parts.url = "github:hercules-ci/flake-parts";
+  };
+  outputs = inputs:
+    inputs.parts.lib.mkFlake {inherit inputs;} {
+      perSystem = {pkgs, ...}: {
+        packages = {
+          watch = pkgs.buildGoModule {
+            pname = "watch";
+            src = ./.;
+            vendorHash = null;
+            version = "0.1.0";
+          };
+        };
+      };
+      systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
+    };
+}
