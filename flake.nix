3d547f236c2c733d8c0b90bbe67fc4fcadc7d1fa
Matthew Dargan <matthewdargan57@gmail.com>
2024-04-06 16:50:19 -0500

    watch: advance to go1.22.2

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
             pname = "watch";
             src = ./.;
             vendorHash = null;
-            version = "0.1.2";
+            version = "0.1.3";
           };
         };
       };
